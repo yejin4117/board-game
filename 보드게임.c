8c3039b155adc681abd28b8b8575966d97dfd1ea
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-05-11 10:10:29 +0900

    엔딩 설정

--- 보드게임.c
+++ 보드게임.c
@@ -28,6 +28,8 @@ void main(void)
 	//무한반복
 	while (1)
 	{
+		//엔터키를 눌러서 진행시켜
+		getchar();
 
 		/*던짐*/
 		//주사위는 1~6까지 나오게 설정
@@ -36,7 +38,11 @@ void main(void)
 		//던진 주사위 값을 현재 위치에 적용
 		position += dice;
 		printf("현재위치 : %d\n\n", position);
-		getchar();
+
+		if (position > 60)
+		{
+			printf("축하합니다. 목적지에 도착하셨습니다.\n");
+		}
 
 	}
 }

734d6179d0d7479bddfaed965d1f831971267caf
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-06-08 08:57:30 +0900

    [문법 공부]반복문 다시연습1

--- 07-03 반복문.c
+++ 07-03 반복문.c
@@ -2,8 +2,15 @@
 
 void main(void)
 {
-	for (int num = 0; num < 5; num++)
-	{
-		printf("예예예 예예예 예예예 예예예\n");
-	}
+	printf("1\n");
+	printf("2\n");
+	printf("3\n");
+	printf("4\n");
+	printf("5\n");
+	printf("6\n");
+	printf("7\n");
+	printf("8\n");
+	printf("9\n");
+	printf("10\n");
+
 }

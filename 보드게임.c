f6f9e120d63319db5f95c2ef8792278f5a46e50f
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-06-08 10:18:15 +0900

    무인도(위치 49)에 있을 때 주사위 3을 던지면 탈출하는 기능 추가

--- 보드게임.c
+++ 보드게임.c
@@ -82,6 +82,14 @@ void main(void)
 			{
 				getchar();
 				printf("%d 턴입니다.",i);
+				dice = rand() % 6 + 1;
+				//주사위를 던져서 3이나오면 무인도를 탈출
+				printf("주사위가 %d가 나왔습니다.\n", dice);
+				if (dice == 3)
+				{
+					printf("축하합니다 %d가 나왔습니다.\n", dice);
+					break;
+				}
 			}
 			printf("무인도에서 탈출하셨습니다.");
 			printf("현재위치 : %d, 현재금액 : %d \n\n", position, money);

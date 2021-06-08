cdcb263d7668537fc6e875a9c460b9f1d74c8bd3
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-06-08 10:22:00 +0900

    무인도 주사위를 전용 주사위로 대체(변수 새로 할당)

--- 보드게임.c
+++ 보드게임.c
@@ -82,9 +82,9 @@ void main(void)
 			{
 				getchar();
 				printf("%d 턴입니다.",i);
-				dice = rand() % 6 + 1;
+				int exit_dice = rand() % 6 + 1;
 				//주사위를 던져서 3이나오면 무인도를 탈출
-				printf("주사위가 %d가 나왔습니다.\n", dice);
+				printf("주사위가 %d가 나왔습니다.\n", exit_dice);
 				if (dice == 3)
 				{
 					printf("축하합니다 %d가 나왔습니다.\n", dice);

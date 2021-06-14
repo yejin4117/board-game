065caf9a4edb0ce86424769c1e623f2500258d04
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-06-14 22:20:36 +0900

    [문법연습] 구구단 2단 간단하게2

--- 구구단.c
+++ 구구단.c
@@ -2,13 +2,13 @@
 
 void main(void)
 {
-	printf("2X1 =2\n");
-	printf("2X2 =4\n");
-	printf("2X3 =6\n");
-	printf("2X4 =8\n");
-	printf("2X5 =10\n");
-	printf("2X6 =12\n");
-	printf("2X7 =14\n");
-	printf("2X8 =16\n");
-	printf("2X9 =18\n");
+	printf("2X1 = %d\n", 2 * 1);
+	printf("2X2 = %d\n", 2 * 2);
+	printf("2X3 = %d\n", 2 * 3);
+	printf("2X4 = %d\n", 2 * 4);
+	printf("2X5 = %d\n", 2 * 5);
+	printf("2X6 = %d\n", 2 * 6);
+	printf("2X7 = %d\n", 2 * 7);
+	printf("2X8 = %d\n", 2 * 8);
+	printf("2X9 = %d\n", 2 * 9);
 }

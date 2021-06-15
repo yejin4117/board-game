3b8de6bc46ad1d11b25a05809f7be4573344255d
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-06-15 09:37:27 +0900

    [문법연습]구구단 완성

--- 구구단.c
+++ 구구단.c
@@ -2,11 +2,12 @@
 
 void main(void)
 {
-	for (int dan = 2; dan <= 3; dan++)
+	for (int dan = 2; dan <= 9; dan++)
 	{
 		for (int i = 1; i <= 9; i++)
 		{
 			printf("%d X %d = %d\n", dan, i, dan * i);
 		}
+		printf("\n");
 	}
 }

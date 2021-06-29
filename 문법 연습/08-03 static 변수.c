bc8b01be8d7e31033721c785beb564a2547e8515
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-06-29 09:20:38 +0900

    static 변수는 프로그램이 끝날때까지 공간이 유지된다

--- 문법 연습/08-03 static 변수.c
+++ 문법 연습/08-03 static 변수.c
@@ -8,9 +8,17 @@ void local_increase(void)
 	a++;
 }
 
+void static_increase(void)
+{
+	static int a = 0;
+	printf("지역변수 %d\n", a);
+	a++;
+}
+
 void main(void)
 {
 	for (int i = 1; i <= 5; i++) {
-		local_increase();
+		//local_increase();
+		static_increase();
 	}
 }

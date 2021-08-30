77948a4685f7ce06bde4fff64b10f252636d0741
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-08-31 08:51:33 +0900

    여러과목 추가

--- 성적처리.c
+++ 성적처리.c
@@ -1,19 +1,40 @@
 #include<stdio.h>
 
-	int main(void)
+int main(void)
 {
 	int score;
-	printf("점수를 입력하시오 : ");
+	printf("씨언어 점수를 입력하시오 : ");
 	scanf_s("%d", &score);
-
 	if (100 >= score && 90 <= score)
-		printf("A");
+		printf("A\n");
 	if (89 >= score && 80 <= score)
-		printf("B");
+		printf("B\n");
 	if (79 >= score && 70 <= score)
-		printf("C");
+		printf("C\n");
 	if (69 >= score)
-		printf("D");
+		printf("D\n");
+
+	printf("자바 점수를 입력하시오 : ");
+	scanf_s("%d", &score);
+	if (100 >= score && 90 <= score)
+		printf("A\n");
+	if (89 >= score && 80 <= score)
+		printf("B\n");
+	if (79 >= score && 70 <= score)
+		printf("C\n");
+	if (69 >= score)
+		printf("D\n");
+
+	printf("컴구 점수를 입력하시오 : ");
+	scanf_s("%d", &score);
+	if (100 >= score && 90 <= score)
+		printf("A\n");
+	if (89 >= score && 80 <= score)
+		printf("B\n");
+	if (79 >= score && 70 <= score)
+		printf("C\n");
+	if (69 >= score)
+		printf("D\n");
 
 	return 0;
 }

b0209737d634c240c5486b8202f9fd2a5235cd52
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-09-07 09:13:52 +0900

    반복문을 효율적으로

--- 문법 연습/10 - 03 문자열 예제2.c
+++ 문법 연습/10 - 03 문자열 예제2.c
@@ -2,9 +2,13 @@
 
 void main(void)
 {
-	char str[20] = "Hello World";
+	//NULL문자('\0')가 포함되어서 12개의 공간을 할당
+	char str[] = "Hello World";
+	printf("%d\n", sizeof(str));
 
-	//단순하게 인덱스 값을 직접 계산함
-	for (int i = 0;  i < 12; i++)
+	//문자열의 갯수를 세지 않고 NULL문자 여부만을 확인
+	for (int i = 0;  str[i] != '\0'; i++)
 		printf("%c", str[i]);
+
+	
 }

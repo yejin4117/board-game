2f91b6a4734ee77186b80207d6b3484f73992cdc
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-09-13 13:58:49 +0900

    일반 int변수를 10진수로

--- 문법 연습/11-01 포인터 예제1.c
+++ 문법 연습/11-01 포인터 예제1.c
@@ -5,7 +5,7 @@ void main(void)
 	int num = 3;
 	int* ptr = &num;
 
-	printf("num안에 들어있는 값 %p \n", num);
-	printf("num의 메모리 주소값 %p \n", &num);
-	printf("ptr안에 들어있는 값 %p \n", ptr);
+	printf("num안에 들어있는 값 %d \n", num);
+	printf("num의 메모리 주소값 %d \n", &num);
+	printf("ptr안에 들어있는 값 %d \n", ptr);
 }

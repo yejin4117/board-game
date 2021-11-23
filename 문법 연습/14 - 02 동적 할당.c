303709ea1f99d81b1865d4ec26f7c09c304990b2
yejin4117 <83990999+yejin4117@users.noreply.github.com>
2021-11-23 09:18:52 +0900

    정적할당된 배열 arr과 동적할당된 배열 ptr

--- 문법 연습/14 - 02 동적 할당.c
+++ 문법 연습/14 - 02 동적 할당.c
@@ -2,13 +2,17 @@
 
 void main(void) 
 {
-	//정적할당 (컴파일 시간에 16bytes로 결정)
+	//정적할당 (컴파일 시간에 16bytes로 결정, stack 영역에 할당되는 지역변수)
 	int arr[4] = { 10, 20,30, 40 };
 
-	int length;
-	printf("배열의 길이를 입력하세요 : ");
-	scanf("%d", &length);
+	int length = 4;
 
-	//실행시간에 메모리가 결정되는 동적할당
-	void* ptr = malloc(length);
+	//UNUSED : 동적할당을 적나라하게 보여주기 위한 코드
+	//printf("배열의 길이를 입력하세요 : ");
+	//scanf("%d", &length);
+
+	//실행시간에 메모리가 결정되는 동적할당(heap 영역)
+	int* ptr = (int*)malloc(length * sizeof(int));
+
+	ptr[0] = 10, ptr[1] = 20, ptr[2] = 30, ptr[3] = 40;
 }
